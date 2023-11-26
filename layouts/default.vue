47d30b1cde9ac818b8abf5ed9736dc508d188662
David <daccomazzo@gradguard.com>
2023-11-25 17:00:03 -0700

    change subtitle

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1 class="text-3xl font-bold underline text-center">Jingle Jams</h1>
-        <h2 class="text-3xl font-bold underline text-center">A Very Merry Advent-ture</h2>
+        <h2 class="text-3xl font-bold underline text-center">A Very Merry Musical Advent-ture</h2>
     </header>
     <div class="wrapper">
       <slot />
