910cb63d3b2122e0be13ed7c2349307d654ed265
David <daccomazzo@gradguard.com>
2023-11-27 09:50:38 -0700

    change the max-width of layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
+    <div class="wrapper max-w-screen-xl my-0 mx-auto">
     <header class="mb-3">
         <h1 class="text-3xl font-bold underline text-center">Jingle Jams</h1>
         <h2 class="text-3xl font-bold underline text-center">A Very Merry Musical Advent-ture</h2>
     </header>
-    <div class="wrapper">
+    <div class="content-wrapper ">
       <slot />
     </div>
+  </div>
   </template>
